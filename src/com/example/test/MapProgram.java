ef1c89b9b1fd2d4adb9017095b528af787e9ee26
Boldeusz <mnpower@poczta.onet.pl>
2019-11-12 11:55:59 +0100

    wpis 11:55

--- src/com/example/test/MapProgram.java
+++ src/com/example/test/MapProgram.java
@@ -9,16 +9,28 @@ public class MapProgram {
 
 
         Map<String, String> languages = new HashMap<>();
-        languages.put("Java", " a compiled high level, object-oriented, platform independent language");
+        if (languages.containsKey("Java")){
+            System.out.println("Java already exists");
+        }else{
+            languages.put("Java", " a compiled high level, object-oriented, platform independent language");
+            System.out.println("Java successfully added");
+        }
+
         languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
         languages.put("Algol", "an algorithmic language");
-        languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code");
-        languages.put("Lisp", "Therein lies madness");
+        System.out.println(languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
 
-        System.out.println(languages.get("Java"));
+        if (languages.containsKey("Java")){
+            System.out.println("Java is already in the map");
+        }else{
+            languages.put("Java", "another post with java");
+        }
 
-        languages.put("Java", "another post with java");
-        System.out.println(languages.get("Java"));
+       // System.out.println(languages.get("Java"));
+
+
+        //System.out.println(languages.get("Java"));
 
 
 
